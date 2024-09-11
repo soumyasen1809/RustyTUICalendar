db6c5b26e84dc9b0d39f0fecfdfbe3326022f00b
Soumya <soumyasen1809@gmail.com>
2024-09-12 00:14:37 +0200

    [5] Add weather frame render layout
    - Added block for weather layout
    - Splitted month view block into 2 parts now
    
    %SOFTWARE

--- src/calendar_widget.rs
+++ src/calendar_widget.rs
@@ -52,6 +52,12 @@ fn get_appointment_block(day: u32, month: u32, year: i32) -> Block<'static> {
         ))
 }
 
+fn get_weather_block() -> Block<'static> {
+    Block::default()
+        .borders(Borders::ALL)
+        .fg(Color::LightMagenta)
+}
+
 pub fn main_calendar_layout(
     frame: &mut Frame,
     main_layout: &Rc<[Rect]>,
@@ -77,14 +83,22 @@ pub fn main_calendar_layout(
         )
         .split(main_layout[0]);
 
+    let month_weather_layout = Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints([Constraint::Percentage(60), Constraint::Percentage(40)].to_vec())
+        .split(layout[1]);
+
     let calendar_block = get_calendar_title_block(month, year);
     let month_days_block = get_calendar_month_block();
     let appointment_block = get_appointment_block(day, month, year);
+    let weather_block = get_weather_block();
 
     frame.render_widget(calendar_block.clone(), layout[0]);
 
-    frame.render_widget(month_days_block.clone(), layout[1]);
-    frame.render_widget(get_calendar_text(calendar_text), layout[1]);
+    frame.render_widget(month_days_block.clone(), month_weather_layout[0]);
+    frame.render_widget(get_calendar_text(calendar_text), month_weather_layout[0]);
+
+    frame.render_widget(weather_block.clone(), month_weather_layout[1]);
 
     frame.render_widget(appointment_block.clone(), layout[2]);
     frame.render_widget(get_appointment_text(appointment_text), layout[2]);
